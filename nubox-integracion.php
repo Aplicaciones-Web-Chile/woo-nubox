86856362411805aad27e6e6bbe1671e83e2795d2
Juan Jorquera <juan@aplicacionesweb.cl>
2022-02-01 11:26:23 -0300

    Update nubox-integracion.php
    updated version

--- nubox-integracion.php
+++ nubox-integracion.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Nubox Integracion
  * Plugin URI:        http://appstore.nubox.com/
  * Description:       Integracion con API REST de Nubox.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Nubox
  * Author URI:        http://www.nubox.com/
  * WC requires at least: 3.4.0
@@ -37,7 +37,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'NUBOX_INTEGRACION_VERSION', '1.0.0' );
+define( 'NUBOX_INTEGRACION_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
